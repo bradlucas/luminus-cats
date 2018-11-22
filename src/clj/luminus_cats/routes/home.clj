7ff37b05a5adc0e980d00a42480ddd02a96bbc6e
Brad Lucas <brad@beaconhill.com>
2018-11-21 21:23:43 -0500

    /cats page

--- src/clj/luminus_cats/routes/home.clj
+++ src/clj/luminus_cats/routes/home.clj
@@ -2,16 +2,21 @@
   (:require [luminus-cats.layout :as layout]
             [compojure.core :refer [defroutes GET]]
             [ring.util.http-response :as response]
-            [clojure.java.io :as io]))
+            [clojure.java.io :as io]
+            [luminus-cats.cats :as c]))
 
 (defn home-page []
   (layout/render
-    "home.html" {:docs (-> "docs/docs.md" io/resource slurp)}))
+    "home.html" {:home (-> "docs/home.md" io/resource slurp)}))
 
 (defn about-page []
   (layout/render "about.html"))
 
+(defn cats-page []
+  (layout/render "cats.html" {:cats (c/get-links 5)}))
+
 (defroutes home-routes
   (GET "/" [] (home-page))
+  (GET "/cats" [] (cats-page))
   (GET "/about" [] (about-page)))
 
